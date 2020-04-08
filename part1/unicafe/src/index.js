5be6df9ec21a855a47dc90e3f1e6ff8402c736bc
Florian Thoennes <mail@der-florian.de>
2020-04-08 13:28:35 +0200

    step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,6 +7,21 @@ const Button = ({handleClick, text}) => (
 )
 const Stat = ({name, number}) => <p>{name} {number}</p>
 
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+
+  return (
+    <div>
+      <Stat name = "good" number = {good} />
+      <Stat name = "neutral" number = {neutral} />
+      <Stat name = "bad" number = {bad} />
+      <Stat name = "all" number = {total} />
+      <Stat name = "avg" number = {(good - bad) / total} />
+      <Stat name = "pos" number = {good / total} />
+    </div>  
+  )
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -23,12 +38,11 @@ const App = () => {
       <Button handleClick = {() => setNeutral(neutral + 1)} text = "neutral" />
       <Button handleClick = {() => setBad(bad + 1)} text = "bad" />
       <Title title = {statsTitle}  />
-      <Stat name = "good" number = {good} />
-      <Stat name = "neutral" number = {neutral} />
-      <Stat name = "bad" number = {bad} />
-      <Stat name = "all" number = {good + neutral + bad} />
-      <Stat name = "avg" number = {(good-bad)/(good+neutral+bad)} />
-      <Stat name = "pos" number = {good/(good+neutral+bad)} />
+      <Statistics
+        good = {good}
+        neutral = {neutral}
+        bad = {bad}
+      />      
     </div>
   )
 }
